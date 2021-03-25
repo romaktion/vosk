027c89efbd0ebb485b17ef954fd8db3e4163f03a
Roman <romaktion@gmail.com>
2021-03-25 18:04:48 +0300

    multithreading

--- convert_wav_to_wav.py
+++ convert_wav_to_wav.py
@@ -2,23 +2,41 @@
 
 # Author: romaktion@gmail.com
 import glob
+import multiprocessing
 import ntpath
 import os
 import subprocess
 import sys
+from concurrent import futures
 from pathlib import Path
+import word_cutter
+
+
+def convert(files_list):
+    for f in files_list:
+        process = subprocess.run(["ffmpeg", "-i", f, "-ab", "160k", "-ac", "1", "-ar", "16000"
+                                     , os.path.join(out_dir, "conv_" + ntpath.basename(f))]
+                                 , stderr=subprocess.DEVNULL
+                                 , stdout=subprocess.DEVNULL
+                                 , stdin=subprocess.PIPE)
+        if process.returncode != 0:
+            print('Converting wav to wav failed for %s!' % f)
+
 
 walk_dir = sys.argv[1]
 out_dir = sys.argv[2]
 Path(out_dir).mkdir(parents=True, exist_ok=True)
 
 files = list(glob.iglob(walk_dir + '**/*.wav', recursive=True))
+cpu_amount = multiprocessing.cpu_count() \
+    if len(files) >= multiprocessing.cpu_count() \
+    else len(files)
+split_files = word_cutter.split_list(files, cpu_amount)
 
-for f in files:
-    process = subprocess.run(["ffmpeg", "-i", f, "-ab", "160k", "-ac", "1", "-ar", "16000"
-                                 , os.path.join(out_dir, "conv_" + ntpath.basename(f))]
-                             , stderr=subprocess.DEVNULL
-                             , stdout=subprocess.DEVNULL
-                             , stdin=subprocess.PIPE)
-    if process.returncode != 0:
-        print('Converting wav to wav failed for %s!' % f)
+with futures.ThreadPoolExecutor(max_workers=cpu_amount) as executor:
+    process_futures = dict((executor.submit(convert, fl), fl) for fl in split_files)
+    for pf in futures.as_completed(process_futures):
+        if pf.exception() is not None:
+            print('generated an exception: %s' % pf.exception())
+        else:
+            print("Done!")
